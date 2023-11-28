34d90ca5fb44a43e61fa1070595b02f0ab420dbe
HumbleG0d <sergiouni2000@gmail.com>
2023-11-27 22:04:47 -0500

    Add InmuebleService

--- src/main/java/uni/sgia/service/InmuebleService.java
+++ src/main/java/uni/sgia/service/InmuebleService.java
@@ -1,2 +1,29 @@
-package uni.sgia.service;public class InmuebleService {
+package uni.sgia.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uni.sgia.model.Inmueble;
+import uni.sgia.respository.InmuebleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class InmuebleService{
+    @Autowired
+    InmuebleRepository inmuebleRepository;
+
+    public List<Inmueble> getInmueble(){
+        return inmuebleRepository.findAll();
+    }
+
+    public Optional<Inmueble> getInmueble(String id){
+        return inmuebleRepository.findById(id);
+    }
+    public void saveOrUpdate(Inmueble imb){
+        inmuebleRepository.save(imb);
+    }
+    public void delete(String id){
+        inmuebleRepository.deleteById(id);
+    }
 }
