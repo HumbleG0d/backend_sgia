aa3db65823e69396386a96aab390a686ceaee0cf
HumbleG0d <sergiouni2000@gmail.com>
2023-11-27 20:49:34 -0500

    add initial config

---
+++ src/main/java/uni/sgia/SgiaApplication.java
@@ -0,0 +1,13 @@
+package uni.sgia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiaApplication.class, args);
+	}
+
+}
