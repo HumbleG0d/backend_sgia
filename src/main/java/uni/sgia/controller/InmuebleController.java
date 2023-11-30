771adb81ace61231117f66689fe92ab3e95e6f93
HumbleG0d <sergiouni2000@gmail.com>
2023-11-30 12:54:21 -0500

    Add CrossOrigin

--- src/main/java/uni/sgia/controller/InmuebleController.java
+++ src/main/java/uni/sgia/controller/InmuebleController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "api/v1/inmuebles")
+@CrossOrigin(origins = "http://localhost:4200/")
 public class InmuebleController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
