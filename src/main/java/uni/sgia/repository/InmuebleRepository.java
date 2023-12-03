5818cbad95d3e87385b9a69d45a53c80b961ec76
HumbleG0d <sergiouni2000@gmail.com>
2023-12-03 09:04:27 -0500

    Change String to Long

--- src/main/java/uni/sgia/repository/InmuebleRepository.java
+++ src/main/java/uni/sgia/repository/InmuebleRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uni.sgia.model.Inmueble;
 @Repository
-public interface InmuebleRepository  extends JpaRepository<Inmueble , String> {
+public interface InmuebleRepository  extends JpaRepository<Inmueble , Long> {
 }
