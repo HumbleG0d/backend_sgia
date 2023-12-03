91aa20e7073eb330ec3cee4f859e712785359b3d
HumbleG0d <sergiouni2000@gmail.com>
2023-12-03 11:31:25 -0500

    Add the comment of the ImmuebleRepository class

--- src/main/java/uni/sgia/repository/InmuebleRepository.java
+++ src/main/java/uni/sgia/repository/InmuebleRepository.java
@@ -3,6 +3,6 @@ package uni.sgia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uni.sgia.model.Inmueble;
-@Repository
-public interface InmuebleRepository  extends JpaRepository<Inmueble , Long> {
+@Repository //Indica que  esta interfaz es un componente de repositorio.
+public interface InmuebleRepository  extends JpaRepository<Inmueble , Long> { //Interfaz que proporciona metodos para realizar operaciones CRUD.Esta interfaz extiende de JPpaRepsitory y especifica dos parametros : Inmubele -> que es el tipo de entidad con la que trabaja el repositorio  y Long -> es el tipo del campo de clave primaria de la entidad.
 }
