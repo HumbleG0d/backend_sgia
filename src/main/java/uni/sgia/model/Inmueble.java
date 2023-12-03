80401603918f3a1ac3221a88dfe8887279ae75fc
HumbleG0d <sergiouni2000@gmail.com>
2023-12-03 11:26:09 -0500

    Add the comment of the Immuble class

--- src/main/java/uni/sgia/model/Inmueble.java
+++ src/main/java/uni/sgia/model/Inmueble.java
@@ -10,24 +10,24 @@ import lombok.Data;
 
 import java.util.Date;
 
-@Data
-@Entity
-@Table(name = "inmueble")
+@Data //Anotacion de Lombok para generar automaticamente getters , setters
+@Entity //Anotacion de JPA para indicar que esta clase es una entidad persistente
+@Table(name = "inmueble") //Anotacion de JPA para especificar el nombre de la tabla en la base de datos
 public class Inmueble {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; //Cantidad de inmuebles;
+    @Id //Anotacion de JPA para indicar que este campo es la clave primaria
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Generacion automatica para la clave primaria
+    private Long id; //Identificador unico generado automaticamente
     
     private String id_inmueble; //Codigo del inmueble
-    private String inmuebles;
-    private String direccion;
-    private Date fecha_registro = new Date();
+    private String inmuebles; //Tipo de inmueble
+    private String direccion; //Direccion del inmueble
+    private Date fecha_registro = new Date(); //Fecha del registro del inmueble
     
 
 
     @Override
-    public String toString(){
-        return "Inmuebele{"  + inmuebles + ", "  + direccion + " , " + fecha_registro + " ," + id + " }" ;
+    public String toString(){ // Metodo toString personalizado para representar la instacia de la clase como una cadena de texto
+        return "Inmuebele{"  + id + ", "  + id_inmueble + " , " + direccion + " ," + inmuebles + ", " +fecha_registro + " }" ;
     }
 
 }
