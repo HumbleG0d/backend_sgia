b1fca240ac2aeb8f129cc67dd755df9ae2653301
HumbleG0d <sergiouni2000@gmail.com>
2023-12-03 14:07:21 -0500

    Add the comment of the ImmuebleController class

--- src/main/java/uni/sgia/controller/InmuebleController.java
+++ src/main/java/uni/sgia/controller/InmuebleController.java
@@ -19,104 +19,145 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = "api/v1/inmuebles")
-@CrossOrigin(origins = "http://localhost:4200/")
+@RestController //Anotacion que indica que esta clase es un controlador que maneja las solicitudes HTTP.
+@RequestMapping(path = "api/v1/inmuebles") //Anotacion que especifica que raiz del URL para todas la solicitudes manejadas por este controlador.
+@CrossOrigin(origins = "http://localhost:4200/") //Anotacion que permite solicitudes desde un origen diferente, en este caso http://localhost:4200/
 public class InmuebleController {
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private Logger logger = LoggerFactory.getLogger(this.getClass()); //Objeto de registro de Logger utilizado para registrar mensajes y eventos en la aplicacion.
 
-    @Autowired
+    @Autowired //Indica que InmuebleRepository debe ser inyectado automaticamente
     InmuebleService inmuebleService;
 
-    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE) //La anotacion RequestMapping especifica como debe manejarse las solicitudes HTTP entrantes en el metodo getInmuebles.
 
-    public ResponseEntity<List<Inmueble>> getInmuebles() {
+    public ResponseEntity<List<Inmueble>> getInmuebles() { //Metodo que maneja las solicitudes HTTP GET para obtener la lista de inmuebles.Usa inmuebleService.getInmuebles() para obtener la lista desde el sevicio. Devuelve una respuesta con la lista de inmuebles y un codigo de estado HTTP.
         logger.info("> getInmuebles[Inmueble]");
-        List<Inmueble> list = null;
+        List<Inmueble> list = null; //Incializamos la lista de inmuebles
 
+        //Usamos try catch para el manejo de excepciones en caso de error al obtener la lista de inmuebles.
         try {
+            //Obtenemos la lista de inmuebles del servicio
             list = inmuebleService.getInmuebles();
-            if (list == null)
+            if (list == null) //Si la lista es nula , se inicializa como una lista vacia
                 list = new ArrayList<>();
         } catch (Exception e) {
-            logger.error("Excepcion inesperada al obtener la lista de personas", e);
-            return new ResponseEntity<>(list, HttpStatus.INTERNAL_SERVER_ERROR);
+            logger.error("Excepcion inesperada al obtener la lista de personas", e); //Registra un mensaje de error en el sistema de registro
+            return new ResponseEntity<>(list, HttpStatus.INTERNAL_SERVER_ERROR); //Devuelve una respuesta con la lista nula y un codigo de estado HTTP 500(Error interno del servidor)
         }
-        logger.info("> getPersonas[Persona]");
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        logger.info("> getPersonas[Persona]"); //Registramos el mensaje de informacion en el sistema de registro
+        return new ResponseEntity<>(list, HttpStatus.OK); //Devuelce una respuesta con la lista de inmuebles y un codigo de estado HTTP 200.
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 
-    public ResponseEntity<Inmueble> addInmueble(@RequestBody Inmueble inmueble) {
-        logger.info(" >agregar: " + inmueble.toString());
+    public ResponseEntity<Inmueble> addInmueble(@RequestBody Inmueble inmueble) { //Metodo que maneja las solicitudes HTTP POST para gregar un uevo inmueble. Utiliza inmuebleService.saveOrUpdate(inmueble) para guardar el inmueble en la base de datos. Devuelve una respuesta con el inmueble agregado y un codigo de estado HTTP
+        // Registra un mensaje de información en el sistema de registro.
+    logger.info(" >agregar: " + inmueble.toString());
 
-        try {
-            inmuebleService.saveOrUpdate(inmueble);
-        } catch (Exception e) {
-            logger.error("Excepcion inesperada al agregar inmuble", e);
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(inmueble, HttpStatus.OK);
+    // Usamos try catch para el manejo de excepciones en caso de error al agregar el inmueble.
+    try {
+        // Intenta guardar o actualizar el inmueble utilizando el servicio.
+        inmuebleService.saveOrUpdate(inmueble);
+    } catch (Exception e) {
+        // Registra un mensaje de error en el sistema de registro.
+        logger.error("Excepción inesperada al agregar inmueble", e);
+
+        // Devuelve una respuesta con el inmueble nulo y un código de estado HTTP 500 (Error interno del servidor).
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Devuelve una respuesta con el inmueble agregado y un código de estado HTTP 200 (OK).
+    return new ResponseEntity<>(inmueble, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 
-    public ResponseEntity<Inmueble> searchInmueble(@RequestParam(name = "id") Long id) {
-        logger.info(">getInmueble id_inmueble: " + id);
+    public ResponseEntity<Inmueble> searchInmueble(@RequestParam(name = "id") Long id) { //Metodo que maneja las solicitudes HTTP GET para buscar un inmueble por su ID. Utiliza inmuebleService.getInmueble(id) para obtener el inmueble. Devuelve una respuesta con el inmueble encontrado o un codigo de estado HTTP de "no encontrado"
+        // Registra un mensaje de información en el sistema de registro.
+    logger.info(">getInmueble id_inmueble: " + id);
 
-        try {
-            Optional<Inmueble> inmueble = inmuebleService.getInmueble(id);
-            if (inmueble.isPresent()) {
-                return new ResponseEntity<>(inmueble.get(), HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            logger.error("Excepcion inesperada al obtener un inmueble", e);
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    // Usamos try catch para el manejo de excepciones en caso de error al obtener el inmueble.
+    try {
+        // Intenta obtener el inmueble del servicio utilizando su ID.
+        Optional<Inmueble> inmueble = inmuebleService.getInmueble(id);
+
+        // Verifica si el inmueble está presente en la opción.
+        if (inmueble.isPresent()) {
+            // Devuelve una respuesta con el inmueble encontrado y un código de estado HTTP 200 (OK).
+            return new ResponseEntity<>(inmueble.get(), HttpStatus.OK);
+        } else {
+            // Devuelve una respuesta con el inmueble nulo y un código de estado HTTP 404 (No encontrado).
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
+    } catch (Exception e) {
+        // Registra un mensaje de error en el sistema de registro.
+        logger.error("Excepción inesperada al obtener un inmueble", e);
 
+        // Devuelve una respuesta con el inmueble nulo y un código de estado HTTP 500 (Error interno del servidor).
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
-
+    }
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
 
-    public ResponseEntity<String> deleteInmueble(@PathVariable Long id) {
-        logger.info(">getInmueble id_inmueble: " + id);
+    public ResponseEntity<String> deleteInmueble(@PathVariable Long id) { //Metodo que maneja las solicitudes HTTP DELETE para eliminar un inmueble por su ID. Utiliza inmuebleService.delete(id) para eliminar el inmueble. Devuelve una respuesta con un mensaje indicando el resultado y un codigo de estado HTTP
+        // Registra un mensaje de información en el sistema de registro.
+    logger.info(">getInmueble id_inmueble: " + id);
 
-        try {
-            inmuebleService.delete(id);
-        } catch (Exception e) {
-            logger.error("Excepcion inesperada al obtener un inmueble", e);
-            return new ResponseEntity<>("Error al eliminar el inmueble", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    // Usamos try catch para el manejo de excepciones en caso de error al eliminar el inmueble.
+    try {
+        // Intenta eliminar el inmueble del servicio utilizando su ID.
+        inmuebleService.delete(id);
+    } catch (Exception e) {
+        // Registra un mensaje de error en el sistema de registro.
+        logger.error("Excepción inesperada al obtener un inmueble", e);
 
-        return new ResponseEntity<>("Inmuble eliminado exitosamente", HttpStatus.OK);
+        // Devuelve una respuesta con un mensaje de error y un código de estado HTTP 500 (Error interno del servidor).
+        return new ResponseEntity<>("Error al eliminar el inmueble", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Devuelve una respuesta con un mensaje indicando el éxito y un código de estado HTTP 200 (OK).
+    return new ResponseEntity<>("Inmueble eliminado exitosamente", HttpStatus.OK);
     }
 
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Inmueble> updateInmueble(@PathVariable Long id, @RequestBody Inmueble inmueble) {
-        logger.info(">updateInmueble id_inmueble: " + id);
+    public ResponseEntity<Inmueble> updateInmueble(@PathVariable Long id, @RequestBody Inmueble inmueble) { //Metodo que maneja las solicitudes HTTP PUT para actualizar un inmueble por su ID. Utiliza inmuebleService.getInmueble(id) para obtener el inmueble existente , actualiza sus propiedades y lo guarda. Devuelve una respuesta con el inmueble actualizado o un codigo de estado HTTP de "no encontrado"
+        // Registra un mensaje de información en el sistema de registro.
+    logger.info(">updateInmueble id_inmueble: " + id);
 
-        try {
-            Optional<Inmueble> existingInmueble = inmuebleService.getInmueble(id);
-            if (existingInmueble.isPresent()) {
-                Inmueble updatedInmueble = existingInmueble.get();
-                updatedInmueble.setInmuebles(inmueble.getInmuebles());
-                updatedInmueble.setDireccion(inmueble.getDireccion());
-                // Actualiza otros campos según sea necesario
+    // Usamos try catch para el manejo de excepciones en caso de error al actualizar el inmueble.
+    try {
+        // Intenta obtener el inmueble existente del servicio utilizando su ID.
+        Optional<Inmueble> existingInmueble = inmuebleService.getInmueble(id);
 
-                inmuebleService.saveOrUpdate(updatedInmueble);
+        // Verifica si el inmueble existente está presente en la opción.
+        if (existingInmueble.isPresent()) {
+            // Obtiene el inmueble existente.
+            Inmueble updatedInmueble = existingInmueble.get();
 
-                return new ResponseEntity<>(updatedInmueble, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            logger.error("Excepcion inesperada al actualizar un inmueble", e);
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            // Actualiza las propiedades del inmueble con los valores proporcionados en el cuerpo de la solicitud.
+            updatedInmueble.setInmuebles(inmueble.getInmuebles());
+            updatedInmueble.setDireccion(inmueble.getDireccion());
+            // Puedes agregar líneas adicionales para actualizar otros campos según sea necesario.
+
+            // Guarda el inmueble actualizado en la base de datos.
+            inmuebleService.saveOrUpdate(updatedInmueble);
+
+            // Devuelve una respuesta con el inmueble actualizado y un código de estado HTTP 200 (OK).
+            return new ResponseEntity<>(updatedInmueble, HttpStatus.OK);
+        } else {
+            // Devuelve una respuesta con el inmueble nulo y un código de estado HTTP 404 (No encontrado)
+            // si el inmueble existente no está presente.
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
+    } catch (Exception e) {
+        // Registra un mensaje de error en el sistema de registro.
+        logger.error("Excepción inesperada al actualizar un inmueble", e);
+
+        // Devuelve una respuesta con el inmueble nulo y un código de estado HTTP 500 (Error interno del servidor)
+        // en caso de error.
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     }
 
 }
